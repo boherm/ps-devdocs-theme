a7cb92e727b0acd7fca716cb866c5a2ea602e398
Thomas Nares <thomas-nares.ext@prestashop.com>
2022-12-13 17:14:17 +0400

    Add capability to search by a full hookname on a dynamic hook name

--- static/js/hookFilter.js
+++ static/js/hookFilter.js
@@ -19,14 +19,31 @@ $(function() {
         });
     };
 
+    /* search must be bi-directional : 
+        - we can type a regex pattern and match against the hook name, 
+        - or our search query can be matched against a dynamic hook name
+        This function is made for this scenario : 
+        we replace the dynamic parts of the hookname with a regex pattern. 
+    */
+    const matchSearchWithDynamicHookName = function(search, hookName){
+        const regex = new RegExp("\<(.*)\>", 'i');
+        const regexedHookName = hookName.replace(regex, "(.*)");
+        if(regexedHookName != hookName){
+            const hookRegex = new RegExp(regexedHookName, 'i');
+            return search.match(hookRegex);
+        }
+        return false;
+    }
+
     const filterList = function(search) {
         let count = 0;
         const regex = new RegExp(escapeRegExp(search), 'i');
-
+        
         allItems.each(function() {
             const $item = $(this);
             const text = $item.text();
-            const matchesSearch = Boolean(text.match(regex));
+            const hookName = $item.find('a.hookTitle').text();
+            const matchesSearch = Boolean(matchSearchWithDynamicHookName(search, hookName) || text.match(regex));
             $item.toggle(matchesSearch);
 
             // to be removed when 1.7 doc is deleted
